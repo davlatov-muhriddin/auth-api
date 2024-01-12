27a780ec815fc6f46b5d6ccbf09ca3c098607dde
muhriddind <muhriddind95@gmail.com>
2024-01-12 20:17:47 +0500

    auth

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Frontendning URL manzili
+    origin: "https://auth-react-zeta.vercel.app/", // Frontendning URL manzili
     credentials: true,
   })
 );
